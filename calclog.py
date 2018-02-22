21b06393341dcd7792481bf9fe92780a3c7a6eb8
Kazon Wilson <kazonwilson@gmail.com>
2018-02-21 22:15:48 -0600

    Changed logger format/fixed bug where miner would restart everytime it looped
    Changed logger format

--- calclog.py
+++ calclog.py
@@ -8,14 +8,14 @@ date = datetime.now().strftime('%m-%d-%Y')
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d-%Y %H:%M',
+                    datefmt='%m-%d-%Y %I:%M%p',
                     filename= FILEPATH + '/logs/calc' + date + '.log',
                     filemode='a')
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 # set a format which is simpler for console use
-formatter = logging.Formatter('%(asctime)s %(name)s: %(levelname)s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%m-%d-%Y %I:%M%p')
 # tell the handler to use this format
 console.setFormatter(formatter)
 # add the handler to the root logger
